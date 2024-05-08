4467bdab4c815b59ac78e3deba39db5ff9d8e2a9
anish42borkar <anishborkar42@gmail.com>
2024-05-08 09:59:27 +0530

    change sidebar theme to match ui

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { list } from "./navbar";
 import Link from "next/link";
 import { useSidebarContext } from "@/provider/sidebar-provider";
+import Button from "./button";
 
 const Sidebar = () => {
   const { sidebar, toggle } = useSidebarContext();
@@ -11,12 +12,12 @@ const Sidebar = () => {
     <div
       className={`w-60 h-screen fixed  ${
         sidebar ? "translate-x-0" : "-translate-x-[100vw]"
-      } top-0 bg-white z-40`}
+      } top-0 bg-[#00072d] text-white z-40`}
     >
       <Image
-        width={40}
-        height={40}
-        className=" absolute right-0 cursor-pointer"
+        width={20}
+        height={20}
+        className="burger absolute top-2 right-0 cursor-pointer"
         src="/assets/icons/close.svg"
         alt="logo"
         onClick={() => toggle()}
@@ -27,11 +28,15 @@ const Sidebar = () => {
           <Link
             key={link.title}
             href={"#" + link.ref}
-            className=" text-black px-2 py-4 hover:bg-gray-500/30"
+            className="  px-2 py-4 hover:bg-gray-500/30"
           >
             {link.title}
           </Link>
         ))}
+
+        <div className="self-center">
+          <Button label="BUY TICKETS" icon="/assets/icons/arrow2.png" />
+        </div>
       </div>
     </div>
   );
